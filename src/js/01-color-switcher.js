b696ee8434e566c06eeee37c71dfcf1c7ad53eb8
IrynaVal <piv8050@gmail.com>
2022-12-29 19:47:18 +0200

    task1 added

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector("body");
 
+stopBtn.disabled = true;
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+function onStartClick() {
+    changeColor();
+    const timerId = setTimeout(changeColor, 0);
+    intervalId = setInterval(() => {
+        changeColor();
+    }, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+}
+
+function changeColor() {
+    return body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStopClick() {
+    clearInterval(intervalId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+}
