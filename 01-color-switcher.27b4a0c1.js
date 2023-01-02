43c746a7dab26b68b434b31c3cc0d9d550d72c3f
IrynaVal <110104641+IrynaVal@users.noreply.github.com>
2023-01-02 20:16:58 +0000

    Deploying to gh-pages from @ IrynaVal/goit-js-hw-09@e3e379e4ed6d76e213c4414d9de5cf9443a4bf89 🚀

--- 01-color-switcher.27b4a0c1.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");function n(){return d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}e.disabled=!0,t.addEventListener("click",(function(){n();setTimeout(n,0);intervalId=setInterval((()=>{n()}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(intervalId),t.disabled=!1,e.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.27b4a0c1.js.map
