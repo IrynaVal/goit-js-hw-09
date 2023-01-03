b6a5c52720ce0ffe67551d0ae78803b0b7b296f6
IrynaVal <piv8050@gmail.com>
2023-01-03 17:58:34 +0200

    test

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,28 @@
+console.log(1);
+
+const formEl = document.querySelector('.form');
+console.log(formEl);
+form.addEventListener('submit', createPromise);
+form.addEventListener('input', callback);
+
+
+
+
+const promise = new Promise((resolve, reject) => {
+  const shouldResolve = Math.random() > 0.3;
+  setTimeout(() => {
+     if (shouldResolve) {
+    // Fulfill
+  } else {
+    // Reject
+  }
+  }, 1000)
+});
+console.log(promise)
+
 function createPromise(position, delay) {
+  // preventDefault();
+
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
@@ -6,3 +30,11 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
