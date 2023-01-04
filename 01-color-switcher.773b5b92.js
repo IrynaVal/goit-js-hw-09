5bf6015340890a127e734516feedad65add00766
IrynaVal <110104641+IrynaVal@users.noreply.github.com>
2023-01-04 16:10:05 +0000

    Deploying to gh-pages from @ IrynaVal/goit-js-hw-09@7299090d14aa9fccf4973166c29183b865224a84 🚀

--- 01-color-switcher.773b5b92.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");let d=null;function o(){return n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}e.disabled=!0,t.addEventListener("click",(function(){o();setTimeout(o,0);d=setInterval((()=>{o()}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.773b5b92.js.map
--- 03-promises.027f0bb9.js
+++
@@ -1,2 +0,0 @@
-!function(){console.log(1);var o=document.querySelector(".form");console.log(o),form.addEventListener("submit",e),form.addEventListener("input",callback);var n=new Promise((function(o,n){Math.random();setTimeout((function(){}),1e3)}));function e(o,n){Math.random()}console.log(n),e().then((function(o){var n=o.position,e=o.delay;console.log("✅ Fulfilled promise ".concat(n," in ").concat(e,"ms"))})).catch((function(o){var n=o.position,e=o.delay;console.log("❌ Rejected promise ".concat(n," in ").concat(e,"ms"))}))}();
-//# sourceMappingURL=03-promises.027f0bb9.js.map
